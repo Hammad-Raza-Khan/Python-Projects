ebdf12f26179642985661759f92935b9500b9cf2
Hammad Raza Khan <hammadrazakhann@gmail.com>
2024-03-14 12:37:41 +0530

    7th Project

---
+++ Day_01.py
@@ -0,0 +1,12 @@
+# Halal Name Generator
+
+#1. Create a greeting for your program.
+print("Assalamu Alaikum, Welcome to the Band Name Generator")
+#2. Ask the user for the city that they grew up in.
+city = input("What is the name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their name.
+print("Your band name could be " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line:
+print("\n")
